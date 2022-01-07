389f38de236eb76e7819239f3c49996534aa7c2a
Tsunder <Tsunder@users.noreply.github.com>
2022-01-06 22:06:48 -0500

    Update NetscriptDefinitions.d.ts

--- src/ScriptEditor/NetscriptDefinitions.d.ts
+++ src/ScriptEditor/NetscriptDefinitions.d.ts
@@ -1728,15 +1728,15 @@ export interface Singularity {
    * This function returns true if you successfully start working on the specified program, and false otherwise.
    *
    * Note that creating a program using this function has the same hacking level requirements as it normally would. These level requirements are:
-   * * BruteSSH.exe: 50
-   * * FTPCrack.exe: 100
-   * * relaySMTP.exe: 250
-   * * HTTPWorm.exe: 500
-   * * SQLInject.exe: 750
-   * * DeepscanV1.exe: 75
-   * * DeepscanV2.exe: 400
-   * * ServerProfiler.exe: 75
-   * * AutoLink.exe: 25
+   * \* BruteSSH.exe: 50
+   * \* FTPCrack.exe: 100
+   * \* relaySMTP.exe: 250
+   * \* HTTPWorm.exe: 500
+   * \* SQLInject.exe: 750
+   * \* DeepscanV1.exe: 75
+   * \* DeepscanV2.exe: 400
+   * \* ServerProfiler.exe: 75
+   * \* AutoLink.exe: 25
    *
    * @example
    * ```ts
